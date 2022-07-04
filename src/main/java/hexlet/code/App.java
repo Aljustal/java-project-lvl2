9cd816cba71f2dd094ca7721b6c732966dcbbe21
Aljustal <orbit.fsk@yandex.ru>
2022-07-04 15:43:45 +0300

    add picocli library

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,7 +1,31 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true,
+        description = "Compares two configuration files and shows a difference.")
+
+class App implements Callable<Integer> {
+
+    @Parameters(description = "path to first file")
+    private String  filepath1;
+
+    @Parameters(description = "path to second file")
+    private String  filepath2;
+
+    @Option(names = {"-f", "--format "}, defaultValue = "stylish", description = "output format [default: stylish]")
+    private String format;
+
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+    @Override
+    public Integer call() throws Exception {
+        return 0;
     }
 }
