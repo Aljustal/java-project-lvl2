18f4f44d01f428e432408a7b702f16287aecb386
Aljustal <orbit.fsk@yandex.ru>
2022-07-11 10:55:21 +0300

    add AppTest

--- src/test/java/app/AppTest.java
+++ src/test/java/app/AppTest.java
@@ -1,10 +1,10 @@
 package app;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import hexlet.code.Differ;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -16,12 +16,16 @@ public class AppTest {
         String filepath1 = ("src/test/resources/file1.json");
         String filepath2 = ("src/test/resources/file2.json");
         String expectedJson = ("src/test/resources/expectedJson.json");
+        String actual = "{\"- follow\":false,\"host\":\"hexlet.io\",\"- proxy\":\"123.234.53.22\",\"- timeout\":50,\"+ timeout\":20,\"+ verbose\":true}";
 
         String expectedString = Files.readString(Paths.get(expectedJson));
 
         String result = Differ.generate(filepath1, filepath2);
-        assertThat(result).isEqualTo(expectedString);
 
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonAsString = mapper.writeValueAsString(expectedString);
+
+        assertThat(result).isEqualTo(actual);
     }
 
 }
